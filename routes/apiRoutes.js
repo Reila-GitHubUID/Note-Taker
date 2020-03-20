36984ca5ca6c5a294d2281f259f72ee95d15fff4
Ellin Tj <tjellin@gmail.com>
2020-03-19 23:25:48 -0700

    made the API GET, POST, and DELETE templates following the readme requirements

---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Request
+  // Below code reads the `db.json` file and return all saved notes as JSON.
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  // API POST Request
+  // Below code receives a new note to save on the request body, add it to 
+  // the `db.json` file, and then return the new note to the client.
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function(req, res) {
+
+  });
+
+  // API DELETE Request
+  // Below code receives a query parameter containing the id of a note to 
+  // delete. This means you'll need to find a way to give each note a unique `id` 
+  // when it's saved. In order to delete a note, you'll need to read all notes 
+  // from the `db.json` file, remove the note with the given `id` property, and 
+  // then rewrite the notes to the `db.json` file.
+  // ---------------------------------------------------------------------------
+  app.delete("/api/notes/:id", function(req, res) {
+
+  });
+};
