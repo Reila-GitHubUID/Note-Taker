6b4f2e5a4da30942fb5104d0789214ea56c4ffcb
Ellin Tj <tjellin@gmail.com>
2020-03-21 23:28:30 -0700

    trying to get the save and edit button at index.html to get activated

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -96,7 +96,7 @@ var handleNewNoteView = function() {
 // If a note's title or text are empty, hide the save button
 // Or else show it
 var handleRenderSaveBtn = function() {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+  if ((!$noteTitle.val().trim()) || (!$noteText.val().trim())) {
     $saveNoteBtn.hide();
   } else {
     $saveNoteBtn.show();
@@ -126,7 +126,7 @@ var renderNoteList = function(notes) {
 };
 
 // Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
+var getAndRenderNotes = function() {  
   return getNotes().then(function(data) {
     renderNoteList(data);
   });
@@ -141,3 +141,8 @@ $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
+
+
+console.log("$noteTitle.val().trim() === "+ $noteTitle.val().trim());
+console.log("$noteText.val().trim() === " + $noteText.val().trim());
+console.log("activateNote.id======="+activateNote.id);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -44,6 +44,21 @@ module.exports = function(app) {
     
     try {
 
+      // Checking if there are entries to save or not
+      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+      // It will do this by sending out the value "true" have a table
+      // req.body is available since we're using the body parsing middleware
+      if (tableData.length < 5) {
+        tableData.push(req.body);
+        res.json(true);
+      }
+      else {
+        waitListData.push(req.body);
+        res.json(false);
+      }
+      
+
+      // Append the entry in db.json file
       appendFileAsync("./db/db.json", joke + "\n")
       .then(function() {
         readFileAsync("./db/db.json", "utf8")
